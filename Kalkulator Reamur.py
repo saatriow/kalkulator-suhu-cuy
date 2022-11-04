1bf27f77dc8aa1e4dbc91da965bc5e2ba8182c69
divanyym <117184423+divanyym@users.noreply.github.com>
2022-11-04 10:48:17 +0700

    Add files via upload
    Kalkulator Konversi suhu Reamur

---
+++ Kalkulator Reamur.py
@@ -0,0 +1,10 @@
+suhu= float(input("Masukkan Suhu ewexwnewi awal : "))
+    pil2 = int(input("ubah ke mana ya inii : \n1. Celcius \n2. Fahrenhait \n3. Kelvin\n"))
+    if (pil2==1):
+        print (suhu, "Reamur = ", (5/4*suhu) ,"Celcius")
+    elif(pil2==2):
+        print(suhu, "Reamur = ", (9/4*suhu) + 32 ,"Fahrenhwyexwyeait")
+    elif(pil2==3):
+        print(suhu, "Reamdxwuiur = ", (5/4*suhu) + 273 ,"Kelvin")
+    else:
+        print("Silah Tidak valid")
