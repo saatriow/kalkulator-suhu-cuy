9ff71f2435298f8b6be63223e6024bf7872b7b2e
satriow <m.psatriowibowo@gmail.com>
2022-11-04 10:41:12 +0700

    buat baru cuy

---
+++ kalkulator suhu bagian celcius & farenhait.py
@@ -0,0 +1,25 @@
+print("==========kalkulator suhu==========")
+print("Pilih suhu yang ingin dirubah")
+pil = int(input("1. Celcius \n2. Fahrenhait \n3. Reamur \n4. Kelvin \n"))
+if (pil == 1):
+    suhu= float(input("Masukkan Suhu celcius awal : "))
+    pil2 = int(input("ubah celcius ke : \n1. Fahrenhait \n2. Reamur \n3. Kelvin \n"))
+    if (pil2==1):
+        print(suhu, "celcius = ", (9/5*suhu)+32 ,"Fahrenhait")
+    elif(pil2==2):
+        print(suhu, "celcius = ", (4/5*suhu) ,"Reamur")
+    elif(pil2==3):
+        print(suhu, "celcius = ", suhu + 273 ,"Kelvin")
+    else:
+        print("Tidak valid")
+elif (pil == 2):
+    suhu= float(input("Masukkan Suhu Fahrenhait awal : "))
+    pil2 = int(input("ubah ke : \n1. Celcius \n2. Reamur \n3. Kelvin\n"))
+    if (pil2==1):
+        print(suhu, "Fahrenhait = ", (5/9*(suhu-32)) ,"Celcius")
+    elif(pil2==2):
+        print(suhu, "Fahrehait = ", (4/9*(suhu-32)) ,"Reamur")
+    elif(pil2==3):
+        print(suhu, "Fahrenhait = ", (5/9*(suhu-32) + 273) ,"Kelvin")
+    else:
+        print("Tidak valid")
