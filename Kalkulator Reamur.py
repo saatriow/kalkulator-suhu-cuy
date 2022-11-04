bb6584b302c4efed9a271bc94f1d04954f80bd3b
divanyym <117184423+divanyym@users.noreply.github.com>
2022-11-04 10:56:45 +0700

    Update Kalkulator Reamur.py

--- Kalkulator Reamur.py
+++ Kalkulator Reamur.py
@@ -1,10 +1,10 @@
 suhu= float(input("Masukkan Suhu Awal Reamur yang akan di Konversi : "))
-    pil2 = int(input("ubah ke mana ya inii : \n1. Celcius \n2. Fahrenhait \n3. Kelvin\n"))
+    pil2 = int(input("ubah ke mana ya inii : \n1. Celcius \n2. Fahrenheit \n3. Kelvin\n"))
     if (pil2==1):
         print (suhu, "Reamur = ", (5/4*suhu) ,"Celcius")
     elif(pil2==2):
-        print(suhu, "Reamur = ", (9/4*suhu) + 32 ,"Fahrenhwyexwyeait")
+        print(suhu, "Reamur = ", (9/4*suhu) + 32 ,"Fahrenheit")
     elif(pil2==3):
-        print(suhu, "Reamdxwuiur = ", (5/4*suhu) + 273 ,"Kelvin")
+        print(suhu, "Reamur = ", (5/4*suhu) + 273 ,"Kelvin")
     else:
         print("Silah Tidak valid")
