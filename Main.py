0ef0038f97c3c55fe77327a67500464131c47ec6
divanyym <117184423+divanyym@users.noreply.github.com>
2022-11-04 11:30:30 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,7 +23,17 @@ elif (pil == 2):
         print(suhu, "Fahrenhait = ", (5/9*(suhu-32) + 273) ,"Kelvin")
     else:
         print("Tidak valid")
-        
+    elif (pil==3):
+    suhu= float(input("Masukkan Suhu ewexwnewi awal : "))
+    pil2 = int(input("ubah ke mana ya inii : \n1. Celcius \n2. Fahrenhait \n3. Kelvin\n"))
+    if (pil2==1):
+        print (suhu, "Reamur = ", (5/4*suhu) ,"Celcius")
+    elif(pil2==2):
+        print(suhu, "Reamur = ", (9/4*suhu) + 32 ,"Fahrenhwyexwyeait")
+    elif(pil2==3):
+        print(suhu, "Reamdxwuiur = ", (5/4*suhu) + 273 ,"Kelvin")
+    else:
+        print("Silah Tidak valid")
     elif (pil==4):
     suhu= float(input("Masukkan Suhu Kelvin awal : "))
     pil2 = int(input("ubah ke : \n1. Celcius \n2. Fahrenhait \n3. Reamur\n"))
